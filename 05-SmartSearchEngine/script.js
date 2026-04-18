5126ade31e9971fa7ad61e756b4330ce37b7eb2b
Mehtab <mehtab185198131@gmail.com>
2026-04-19 01:59:33 +0530

    Add various user search functions in script.js

--- 05-SmartSearchEngine/script.js
+++ 05-SmartSearchEngine/script.js
@@ -271,3 +271,207 @@ const noSkill = (data) => {
   return data.some(u => !u.skills || u.skills.length === 0);
 };
 console.log('Empty skill user:', noSkill(users))
+
+
+
+
+// 🟠 LEVEL 3 (findIndex + Advanced) [21–30]
+// Pehla inactive user ka index
+const firstInActiveUserInd = (data) =>{
+  const userIndex = data.findIndex(u => !u.active);
+  return userIndex !== -1 ? userIndex : null;
+};
+
+console.log('first inActive user index: ',firstInActiveUserInd(users));
+// Pehla user jiska salary > 60k uska index
+const firstUserAbouveSalary = (data) =>{
+  const index = data.findIndex(u => u.salary > 60000);
+  return index !== -1 ? index :null
+}
+
+console.log(firstUserAbouveSalary(users));
+// Mumbai ke first user ka index
+const getUserByCity = (data, city) =>{
+  const c = city.trim().toLowerCase()
+  const index =  data.findIndex(u => u.city.toLowerCase() === c);
+  return index !== -1 ? index :null
+}
+console.log(getUserByCity(users ,"mumbai"))
+// "js" skill wale first user ka index
+const hasUserSkill = (data, query) =>{
+  const q = query.trim().toLowerCase();
+  return data.findIndex(u =>
+    u.skills.some(s => s.toLowerCase() === q)
+  );
+}
+
+console.log(hasUserSkill(users,'js'))
+// Koi user hai kya jiska naam duplicate ho
+const hasDuplicateName = (data) =>{
+  let seen = new Set();
+  for(let user of data){
+    const n = user.name.toLowerCase();
+    if(seen.has(n)){
+      return true
+    }
+    seen.add(n)
+  }
+  return false
+}
+
+console.log('duplicate user name :', hasDuplicateName(users))
+// Pehla user jiska age even ho
+const getFirstEvenAgeUser = (data) => {
+  return data.find(u => typeof u.age === "number" && u.age % 2 === 0) || null;
+};
+
+console.log(getFirstEvenAgeUser(users));
+// Sab users ke names unique hain kya
+const getUniqueUsersName = (data) =>{
+    const name = data.map(u => u.name.toLowerCase());
+  return new Set(name).size === name.length;
+}
+console.log('unique users :', getUniqueUsersName(users))
+// Koi user hai kya jiska salary aur age same hai (random logic 😏)
+const hasSalaryAgeSame = (data) =>{
+  return data.some(u => u.salary === u.age);
+}
+console.log('salary and age same :', hasSalaryAgeSame(users))
+
+// Pehla user jiska skill "node" ho
+const getUserBySkillIndex = (data, skill) => {
+  const q = skill.toLowerCase();
+  return data.findIndex(u =>
+    u.skills.some(s => s.toLowerCase() === q)
+  );
+};
+console.log('first node user : ', getUserBySkillIndex(users,"node"))
+// Koi user hai kya jiska naam lowercase me ho
+const hasLowerCaseName = (data) => {
+    return data.some(u => u.name === u.name.toLowerCase());;
+};
+
+console.log(hasLowerCaseName(users));
+
+*/
+
+// 🔴 LEVEL 4 (INTERVIEW LOGIC) [31–40]
+// Pehla user jiska salary sabse zyada hai (without sort 🔥)
+const getFirstHighestEarner = (data) => {
+  if (!data.length) return null;
+
+  return data.reduce((highest, curr) =>
+    curr.salary > highest.salary ? curr : highest
+  );
+};
+
+console.log('first highest paid user: ',getFirstHighestEarner(users))
+
+// Check karo koi user hai kya jiska salary average se zyada ho
+// const hasUserAboveAverageSalary = (data) =>{
+//   if (!data || data.length === 0) return false;
+//   const avgSalary = data.reduce((sum, u) => sum+ u.salary, 0) / data.length || 0;
+//   return data.some(u => u.salary > avgSalary)
+// }
+
+// best 
+const hasUserAboveAverageSalary = (data) => {
+  let total = 0;
+  for (let u of data) total += u.salary;
+
+  const avg = total / data.length;
+
+  return data.some(u => u.salary > avg);
+};
+
+
+console.log(hasUserAboveAverageSalary(users));
+// Pehla user jiska skills me "js" aur "react" dono ho
+const hasUserBySkill = (data, f, s) => {
+  const [first, second] = [f, s].map((str) => str.trim().toLowerCase());
+  return data.find((u) => {
+    const lowerSkills = u.skills.map((s) => s.toLowerCase());
+    return lowerSkills.includes(first) && lowerSkills.includes(second);
+  });
+};
+console.log("User has both skills:", hasUserBySkill(users, "js", "react"));
+
+// Koi user hai kya jo active bhi hai aur 30+ bhi
+const getActiveUserAboveAge = (data) => {
+  return data.some((u) => u.active && u.age > 30);
+};
+console.log("any active user and age above 30 :", getActiveUserAboveAge(users));
+// Sab users Delhi se hain kya
+const allUserByCity = (data, query) => {
+  const c = query.trim().toLowerCase();
+  return data.every((u) => u.city.toLowerCase() === c);
+};
+
+console.log("all user from same city: ", allUserByCity(users, "delhi"));
+// Pehla user jiska salary 30k–60k range me ho
+const getUserBySalary = (data, min, max) => {
+  return data.find((u) => u.salary >= min && u.salary <= max);
+};
+
+const result = getUserBySalary(users, 30000, 60000);
+
+if (result) {
+  console.log(`Found User: ${result.name} with salary ${result.salary}`);
+} else {
+  console.log("No user found in this range.");
+}
+// Koi user hai kya jiska naam vowel se start hota hai
+const getNameStartVowels = (data) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  return data.some(u =>
+    typeof u.name === "string" &&
+    vowels.includes(u.name?.[0]?.toLowerCase())
+  );
+};
+
+console.log('has any user name start with vowels :', getNameStartVowels(users));
+// Sab users ke paas unique skills hain kya
+const areSkillsUnique = (data) => {
+  let seen = new Set();
+  for(let user of data){
+   for(let skill of user.skills){
+     const s = skill.toLowerCase();
+     if(seen.has(s)){
+       return false;
+     }
+     seen.add(s)
+   }
+  }
+   return true;
+};
+
+const areSkillsUniquePerUser = (data) =>{
+  return data.every (u =>{
+    const skill = u.skills.map(s => s.toLowerCase());
+    return new Set(skill).size === skill.length;
+  })
+}
+
+console.log(areSkillsUnique(users));
+console.log(areSkillsUniquePerUser(users));
+// Pehla user jiska salary even number hai
+const getFirstUserEvenSalary = (data) =>{
+  return data.find(u => typeof u.salary === 'number'
+                  && u.salary % 2 === 0
+                  );
+}
+console.log('first user has salary even number: ', getFirstUserEvenSalary(users))
+// Koi user hai kya jiska name palindrome ho
+const hasPalindromeName = (data) =>{
+  return data.some(user => {
+   if (typeof user.name !== "string") return false;
+    const name = user.name.toLowerCase();
+    const reversed = name.split('').reverse().join('');
+    return name === reversed;
+  })
+}
+
+console.log('Has Palindrome Name User : ', hasPalindromeName(users))
+
+
